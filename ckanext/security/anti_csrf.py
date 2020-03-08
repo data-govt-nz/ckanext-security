65dcd7538ee91bbb59622d4cd87adb0cee17334a
ThrawnCA <shell_layer-github@yahoo.com.au>
2020-03-09 07:02:40 +1000

    generate tokens with HMAC instead of random strings, #25
    - This makes it almost impossible for an attacker to forge a token,
    even if they find a way to set a cookie.
    It also provides a verified timestamp so we can easily check the token age.
    In future, it might even be possible to eliminate the cookie altogether,
    so long as sufficient information is contained in the token,
    eg username + timestamp + intended action.

--- ckanext/security/anti_csrf.py
+++ ckanext/security/anti_csrf.py
@@ -1,62 +1,73 @@
+# encoding: utf-8
 """Provides a filter to prevent Cross-Site Request Forgery,
 based on the Double Submit Cookie pattern,
 https://github.com/OWASP/CheatSheetSeries/blob/master/cheatsheets/Cross-Site_Request_Forgery_Prevention_Cheat_Sheet.md#double-submit-cookie
 
 This is integrated in the CSRFMiddleware
 """
-import ckan.lib.base as base
+import hashlib
+import hmac
+import random
 import re
 from re import IGNORECASE, MULTILINE
+import time
+import urllib
 from logging import getLogger
-from ckan.common import request
+import urlparse
+
+from ckan.common import config, g
+import ckan.lib.base as base
 
 LOG = getLogger(__name__)
-
-RAW_RENDER = base.render
-RAW_RENDER_JINJA = base.render_jinja2
-RAW_BEFORE = base.BaseController.__before__
 
 """ Used as the cookie name and input field name.
 """
 TOKEN_FIELD_NAME = 'token'
-""" Used to rotate the token cookie periodically.
-If the freshness cookie doesn't appear, the token cookie is still OK,
-but we'll set a new one for next time.
-"""
-TOKEN_FRESHNESS_COOKIE_NAME = 'token-fresh'
-
-# We need to edit confirm-action links, which get intercepted by JavaScript,
-#regardless of which order their 'data-module' and 'href' attributes appear.
-CONFIRM_LINK = re.compile(r'(<a [^>]*data-module=["\']confirm-action["\'][^>]*href=["\']([^"\']+))(["\'])', IGNORECASE | MULTILINE)
-CONFIRM_LINK_REVERSED = re.compile(r'(<a [^>]*href=["\']([^"\']+))(["\'][^>]*data-module=["\']confirm-action["\'])', IGNORECASE | MULTILINE)
 
 """
 This will match a POST form that has whitespace after the opening tag (which all existing forms do).
 Once we have injected a token immediately after the opening tag,
 it won't match any more, which avoids redundant injection.
 """
-POST_FORM = re.compile(r'(<form [^>]*method=["\']post["\'][^>]*>)([^<]*\s<)', IGNORECASE | MULTILINE)
+POST_FORM = re.compile(r'(<form [^>]*method=["\']post["\'][^>]*>)(\s[^<]*<)',
+                       IGNORECASE | MULTILINE)
 
 """The format of the token HTML field.
 """
-HEX_PATTERN=re.compile(r'^[0-9a-z]+$')
-TOKEN_PATTERN = r'<input type="hidden" name="' + TOKEN_FIELD_NAME + '" value="{token}"/>'
-TOKEN_SEARCH_PATTERN = re.compile(TOKEN_PATTERN.format(token=r'([0-9a-f]+)'))
+TOKEN_VALIDATION_PATTERN = re.compile(r'^[0-9a-z]+![0-9]+/[0-9]+/[-_a-z0-9%]+$',
+                                      IGNORECASE)
+TOKEN_PATTERN = r'<input type="hidden" name="{}" value="{}"/>'
 API_URL = re.compile(r'^/api\b.*')
+CONFIRM_MODULE_PATTERN = r'data-module=["\']confirm-action["\']'
+HREF_URL_PATTERN = r'href=["\']([^"\']+)'
+
+# We need to edit confirm-action links, which get intercepted by JavaScript,
+# regardless of which order their 'data-module' and 'href' attributes appear.
+CONFIRM_LINK = re.compile(r'(<a [^>]*{}[^>]*{})(["\'])'.format(
+    CONFIRM_MODULE_PATTERN, HREF_URL_PATTERN),
+    IGNORECASE | MULTILINE)
+CONFIRM_LINK_REVERSED = re.compile(r'(<a [^>]*{})(["\'][^>]*{})'.format(
+    HREF_URL_PATTERN, CONFIRM_MODULE_PATTERN),
+    IGNORECASE | MULTILINE)
 
 
 def apply_token(html, token):
     """ Rewrite HTML to insert tokens if applicable.
     """
-
-    token_match = TOKEN_SEARCH_PATTERN.search(html)
-    if token_match:
-        token = token_match.group(1)
+    if not POST_FORM.search(html) and not re.search(CONFIRM_MODULE_PATTERN, html):
+        return html
 
     def insert_form_token(form_match):
-        return form_match.group(1) + TOKEN_PATTERN.format(token=token) + form_match.group(2)
+        """ Inject a token into a POST form. """
+        return form_match.group(1) + TOKEN_PATTERN.format(TOKEN_FIELD_NAME, token) + form_match.group(2)
 
     def insert_link_token(link_match):
+        """ Inject a token into a link that uses data-module="confirm-action".
+        These links are picked up by JavaScript and converted into empty POST requests.
+        """
+        if TOKEN_FIELD_NAME + '=' in link_match.group(1):
+            # token already in query string, return unchanged
+            return link_match.group(0)
         if '?' in link_match.group(2):
             separator = '&'
         else:
@@ -73,7 +84,7 @@ def get_cookie_token(request):
     If not, an error will occur.
     """
     token = None
-    if request.cookies.has_key(TOKEN_FIELD_NAME):
+    if TOKEN_FIELD_NAME in request.cookies:
         LOG.debug("Obtaining token from cookie")
         token = request.cookies.get(TOKEN_FIELD_NAME)
     if token is None or token.strip() == "":
@@ -81,37 +92,147 @@ def get_cookie_token(request):
     return token
 
 
+def _get_user():
+    """ Retrieve the current user object.
+    """
+    return g.userobj
+
+
+def _get_safe_username():
+    """ Retrieve the current username with unsafe characters URL-encoded.
+    """
+    return urllib.quote(_get_user().name, safe='')
+
+
+def validate_token(token):
+    """ Verify the integrity of the provided token.
+    It must have the expected format (hash!timestamp/nonce/username),
+    the hash must match the other values,
+    the username must match the current account,
+    and it must not be older than our limit (currently 30 minutes).
+    TODO Make max age configurable.
+    """
+    token_values = read_token_values(token)
+    if 'hash' not in token_values:
+        return False
+
+    expected_hmac = unicode(get_digest(token_values['message']))
+    # use this special comparison method to avoid timing attacks
+    if not hmac.compare_digest(expected_hmac, token_values['hash']):
+        return False
+
+    now = int(time.time())
+    timestamp = token_values['timestamp']
+    # allow tokens up to 30 minutes old
+    if now < timestamp or now - timestamp > 60 * 30:
+        return False
+
+    return token_values['username'] == _get_safe_username()
+
+
+def read_token_values(token):
+    """ Parse the provided token string.
+    Invalid tokens are parsed as empty dicts.
+    """
+    if not TOKEN_VALIDATION_PATTERN.match(token):
+        return {}
+
+    parts = token.split('!', 1)
+    message = parts[1]
+    # limiting to 2 means that even if a username contains a slash, it won't cause an extra split
+    message_parts = message.split('/', 2)
+
+    return {
+        "message": message,
+        "hash": unicode(parts[0]),
+        "timestamp": int(message_parts[0]),
+        "nonce": int(message_parts[1]),
+        "username": message_parts[2]
+    }
+
+
 def get_response_token(request, response):
     """Retrieve the token to be injected into pages.
 
-    This will be retrieved from the 'token' cookie, if it exists and is fresh.
+    This will be retrieved from the 'token' cookie, if it exists and is valid.
     If not, a new token will be generated and a new cookie set.
     """
     # ensure that the same token is used when a page is assembled from pieces
-    if TOKEN_FIELD_NAME in request.cookies and TOKEN_FRESHNESS_COOKIE_NAME in request.cookies:
+    if TOKEN_FIELD_NAME in request.cookies:
         LOG.debug("Obtaining token from cookie")
         token = request.cookies.get(TOKEN_FIELD_NAME)
-        if not HEX_PATTERN.match(token):
-            LOG.debug("Invalid cookie token; making new token cookie")
+        if not validate_token(token) or is_soft_expired(token):
+            LOG.debug("Invalid or expired cookie token; making new token cookie")
             token = create_response_token(response)
     else:
-        LOG.debug("No fresh token found; making new token cookie")
+        LOG.debug("No valid token found; making new token cookie")
         token = create_response_token(response)
     return token
 
 
+def is_soft_expired(token):
+    """Check whether the token is old enough to need rotation.
+    It may still be valid, but it's time to generate a new one.
+
+    The current rotation age is 10 minutes.
+    TODO Make soft-rotation age configurable.
+    """
+    if not validate_token(token):
+        return False
+
+    now = int(time.time())
+    token_values = read_token_values(token)
+
+    return now - token_values['timestamp'] > 60 * 10
+
+
+def _get_secret_key():
+    """ Retrieve the secret key to use in generating secure hashes.
+    Currently this is the Beaker session secret.
+    """
+    return config.get('beaker.session.secret')
+
+
+def get_digest(message):
+    """ Generate a secure (unforgeable) hash of the provided data.
+    """
+    return hmac.HMAC(_get_secret_key(), message, hashlib.sha512).hexdigest()
+
+
 def create_response_token(response):
-    import binascii, os
-    token = binascii.hexlify(os.urandom(32))
-    response.set_cookie(TOKEN_FIELD_NAME, token, secure=True, httponly=True)
-    response.set_cookie(TOKEN_FRESHNESS_COOKIE_NAME, '1', max_age=600, secure=True, httponly=True)
+    """ Generate an unforgeable CSRF token. The format of this token is:
+    hash!timestamp/nonce/username
+    where the hash is a secure HMAC of the other values plus a secret key.
+    """
+    username = _get_safe_username()
+    timestamp = int(time.time())
+    nonce = random.randint(1, 999999)
+    message = "{}/{}/{}".format(timestamp, nonce, username)
+    token = "{}!{}".format(get_digest(message), message)
+
+    _set_response_token_cookie(token)
     return token
 
 
+def set_response_token_cookie(token, response):
+    """ Add a generated token cookie to the HTTP response.
+    The Secure flag will be set if the CKAN site URL uses HTTPS.
+    """
+    site_url = urlparse.urlparse(config.get('ckan.site_url', ''))
+    if site_url.scheme == 'https':
+        LOG.debug("Securing CSRF token cookie for site %s", site_url)
+        secure_cookies = True
+    else:
+        LOG.warn("Site %s is not secure! CSRF token may be exposed!", site_url)
+        secure_cookies = False
+    response.set_cookie(TOKEN_FIELD_NAME, token, secure=secure_cookies, httponly=True)
+
+
 def csrf_fail(message):
-    from flask import abort
+    """ Abort the request and return an error when there is a problem with the CSRF token.
+    """
     LOG.error(message)
-    abort(403, "Your form submission could not be validated")
+    base.abort(403, "Your form submission could not be validated")
 
 
 def get_post_token(request):
@@ -122,7 +243,7 @@ def get_post_token(request):
     it is also acceptable to provide the token as a query string parameter,
     if there is no POST body.
     """
-    if request.environ['webob.adhoc_attrs'].has_key(TOKEN_FIELD_NAME):
+    if TOKEN_FIELD_NAME in request.environ['webob.adhoc_attrs']:
         return request.token
 
     # handle query string token if there are no POST parameters
@@ -132,16 +253,19 @@ def get_post_token(request):
         del request.GET[TOKEN_FIELD_NAME]
         return request.token
 
-    postTokens = request.POST.getall(TOKEN_FIELD_NAME)
-    if not postTokens:
+    post_tokens = request.POST.getall(TOKEN_FIELD_NAME)
+    if not post_tokens:
         csrf_fail("Missing CSRF token in form submission")
-    elif len(postTokens) > 1:
+    elif len(post_tokens) > 1:
         csrf_fail("More than one CSRF token in form submission")
     else:
-        request.token = postTokens[0]
+        request.token = post_tokens[0]
 
     # drop token from request so it doesn't populate resource extras
     del request.POST[TOKEN_FIELD_NAME]
+
+    if not validate_token(request.token):
+        csrf_fail("Invalid token format")
 
     return request.token
 
