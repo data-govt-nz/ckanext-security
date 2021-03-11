efc336c2487ba64def845bf54da012cbe0a5789b
Ian Ward <ian@excess.org>
2021-03-11 12:00:49 -0500

    api for throttle stats, reset on user update

---
+++ ckanext/security/logic/auth.py
@@ -0,0 +1,15 @@
+'''
+Action functions, all sysadmin-only
+'''
+
+def security_throttle_user_reset(context, data_dict):
+    return {'success': False}
+
+def security_throttle_address_reset(context, data_dict):
+    return {'success': False}
+
+def security_throttle_user_show(context, data_dict):
+    return {'success': False}
+
+def security_throttle_address_show(context, data_dict):
+    return {'success': False}
