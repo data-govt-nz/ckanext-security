1040fce461d6f19237fa3ee65e76ad1f3d71dda9
Rabia Sajjad <rabiasajjad.dev@gmail.com>
2021-09-08 10:17:24 -0400

    remove urlmap for perform_reset since no controller available

--- ckanext/security/plugin.py
+++ ckanext/security/plugin.py
@@ -40,7 +40,6 @@ class CkanSecurityPlugin(plugins.SingletonPlugin):
         urlmap.redirect('/user/edit/', '/user/edit')
         urlmap.connect('/user/edit', controller=userController, action='edit')
         urlmap.connect('/user/edit/{id:.*}', controller=userController, action='edit', ckan_icon='cog')
-        urlmap.connect('/user/reset/{id:.*}', controller=userController, action='perform_reset')
         urlmap.connect('/user/reset', controller=userController, action='request_reset')
         return urlmap
 
